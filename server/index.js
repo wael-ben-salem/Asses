73a20c8ff58cb30864450ef5668d4c79ea9bc0f4
Fares-sghaier# <faroussassg007@gmail.com>
2023-04-04 09:09:05 +0100

    ...

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('../database-mysql');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// UNCOMMENT TO START
+// app.use(express.static(__dirname + '/../react-client/dist'));
+
+app.get('/api/phrases', (req, res) => {
+  //TODO - your code here!
+});
+
+//TODO - add additional route handlers as necessary
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
