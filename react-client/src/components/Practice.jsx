dec072036171bd260ad45f77545c574e237de002
Ons Khiari <98515359+ons-khiari@users.noreply.github.com>
2024-06-10 14:18:23 +0100

    step three

--- react-client/src/components/Practice.jsx
+++ react-client/src/components/Practice.jsx
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 
 const Practice = ({ phrases }) => {
   const [phraseIndex, setPhraseIndex] = useState(0);
+  const [showTranslation, setShowTranslation] = useState(false);
 
   const handleNextPhrase = () => {
     setPhraseIndex((prevIndex) => prevIndex + 1);
+    setShowTranslation(false);
   };
 
   const handleStatusClick = () => {
     handleNextPhrase();
   };
-
+  const handleToggleTranslation = () => {
+    setShowTranslation(!showTranslation);
+  };
   if (phrases.length === 0) {
     return <div>No phrases available</div>;
   }
@@ -22,7 +26,15 @@ const Practice = ({ phrases }) => {
       <div className="card">
         <div className="card-kor">{phrase.kor}</div>
         <div className="card-rom">{phrase.rom}</div>
-        <div className="card-eng">{phrase.eng}</div>
+        {showTranslation ? (
+          <div className="card-eng">{phrase.eng}</div>
+        ) : (
+          <div className="card-eng">-------</div>
+        )}
+        <button onClick={handleNextPhrase}>Next Phrase</button>
+        <button onClick={handleToggleTranslation}>
+          {showTranslation ? "Hide Translation" : "Reveal Translation"}
+        </button>
         <button onClick={handleStatusClick}>Not yet</button>
         <button onClick={handleStatusClick}>Almost</button>
         <button onClick={handleStatusClick}>Got it</button>
